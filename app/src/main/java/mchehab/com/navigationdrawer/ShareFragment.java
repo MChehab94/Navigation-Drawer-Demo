7dcd76e4ccdf86579bccd074f2b88a48b5c3a710
Mohamad <lemirmohammad.shehab@lau.edu>
2017-12-16 16:37:25 +0200

    Created a Fragment for each menu item and their corresponding string in strings.xml

---
+++ app/src/main/java/mchehab/com/navigationdrawer/ShareFragment.java
@@ -0,0 +1,23 @@
+package mchehab.com.navigationdrawer;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ShareFragment extends Fragment {
+
+    public ShareFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_share, container, false);
+    }
+}
